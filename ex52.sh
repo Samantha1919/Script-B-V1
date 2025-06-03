9c9a2beabd731805d29cee289966ba861b51c706
Samanthaa <samantha.schif@gmail.com>
2025-06-03 09:05:47 +0200

    txt add

---
+++ ex52.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SAmantha
+#Schifano
+#20 mai 2025
+
+
+shopt -o -s nounset
+
+declare boisson
+declare nmbrGlacons
+
+boisson="$1"
+nmbrGlacons="$2"
+
+echo "Ma boisson préférée est le $boisson , je l'aime servie avec $nmbrGlacons glaçons."
+	
